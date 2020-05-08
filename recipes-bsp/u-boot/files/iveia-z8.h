276af3a304cea5592c75db2116f838334eaf8a53
Brian Silverman <bsilverman@iveia.com>
2020-05-08 17:20:26 -0400

    u-boot: Rejigger env vars...
    ...to make it simpler in development to manually run a specific boot method
    with:
    
    setenv tgt <bootmode>; run boottgt

--- recipes-bsp/u-boot/files/iveia-z8.h
+++ recipes-bsp/u-boot/files/iveia-z8.h
@@ -1,22 +1,21 @@
 #define CONFIG_EXTRA_ENV_SETTINGS \
     "bootenv_addr=0x600000\0" \
-    "fdt_addr=0x700000\0" \
-    "kernel_addr=0x800000\0" \
-    "initrd_addr=0x10000000\0" \
     "bootenv_file=uEnv.txt\0" \
     "boot_targets=mmc0 mmc1 dhcp\0" \
     "loadenv_mmc0=load mmc 0 ${bootenv_addr} ${bootenv_file}\0" \
     "loadenv_mmc1=load mmc 1 ${bootenv_addr} ${bootenv_file}\0" \
     "loadenv_dhcp=dhcp ${bootenv_addr} ${bootenv_file}\0" \
     "importenv=env import -t ${bootenv_addr} ${filesize}\0" \
+    "boottgt=" \
+        "if run loadenv_${tgt}; then " \
+            "run importenv; " \
+            "setenv loadmode ${tgt}; " \
+            "run bootenv_cmd; " \
+        "fi; " \
     "bootseq=" \
-        "for t in ${boot_targets}; do " \
-            "echo Attempting load of ${bootenv_file} from ${t}...; " \
-            "if run loadenv_${t}; then " \
-                "run importenv; " \
-                "setenv loadmode ${t}; " \
-                "run bootenv_cmd; " \
-            "fi; " \
+        "for tgt in ${boot_targets}; do " \
+            "echo Attempting load of ${bootenv_file} from ${tgt}...; " \
+            "run boottgt; " \
         "done; " \
         "echo No boot images found.  Aborting to prompt...\0" \
 
