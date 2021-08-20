8aca6f42401ef31f4ce1085e63908844694e3af3
Rick Rogers <rrogers@iveia.com>
2021-08-20 15:27:58 +0000

    ivinstall: implement JTAG mode for local machine

--- recipes-core/images/files/ivinstall-header.sh
+++ recipes-core/images/files/ivinstall-header.sh
@@ -309,7 +309,6 @@ fi
 # values used.
 #
 if ((MODE==JTAG_MODE)); then
-    [[ -z "$JTAG_REMOTE" ]] && error "JTAG mode currently requires using remote (-J)"
     info "Extracting Image Archive for JTAG mode..."
     extract_archive_to_TMPDIR
     cp "$CMD" $TMPDIR
@@ -327,20 +326,27 @@ if ((MODE==JTAG_MODE)); then
         fi
         add_header jtag/uEnv.ivinstall.txt uEnv.ivinstall.txt.bin
         cp devicetree/$MACHINE.dtb system.dtb
-        scp ${SSH_OPTS} startup.sh.bin uEnv.ivinstall.txt.bin ivinstall.bin jtag/ivinstall.tcl \
-            elf/* boot/*Image rootfs/initrd system.dtb \
-            $JTAG_REMOTE: || error "scp to JTAG_REMOTE failed"
-        # On Windows, if the ivinstall.tcl script is run via ssh and
-        # immediately exits, the JTAG connection becomes hung (process is
-        # halted).  As a workaround, we test for Linux/Windows, and run a short
-        # sleep after running the script.  The sleep must be run AFTER xsdb
-        # exits, but before ssh exits.
-        if ssh ${SSH_OPTS} $JTAG_REMOTE uname &> /dev/null; then
-            # This is Unix - NOTE UNTESTED
-            ssh ${SSH_OPTS} $JTAG_REMOTE xsdb ivinstall.tcl
+        JTAG_FILES="startup.sh.bin uEnv.ivinstall.txt.bin ivinstall.bin jtag/ivinstall.tcl"
+        JTAG_FILES+=" elf/* boot/*Image rootfs/initrd system.dtb"
+        if [ -n "$JTAG_REMOTE" ]; then
+            scp ${SSH_OPTS} $JTAG_FILES $JTAG_REMOTE: || error "scp to JTAG_REMOTE failed"
+            # On Windows, if the ivinstall.tcl script is run via ssh and
+            # immediately exits, the JTAG connection becomes hung (process is
+            # halted).  As a workaround, we test for Linux/Windows, and run a short
+            # sleep after running the script.  The sleep must be run AFTER xsdb
+            # exits, but before ssh exits.
+            if ssh ${SSH_OPTS} $JTAG_REMOTE uname &> /dev/null; then
+                # This is Unix - NOTE UNTESTED
+                ssh ${SSH_OPTS} $JTAG_REMOTE xsdb ivinstall.tcl
+            else
+                # This is Windows - note '&' in Windows is equiv to ';' in Unix
+                ssh ${SSH_OPTS} $JTAG_REMOTE 'xsdb ivinstall.tcl & xsdb -eval "after 1000"'
+            fi
         else
-            # This is Windows - note '&' in Windows is equiv to ';' in Unix
-            ssh ${SSH_OPTS} $JTAG_REMOTE 'xsdb ivinstall.tcl & xsdb -eval "after 1000"'
+            mkdir staging
+            cp $JTAG_FILES staging/
+            cd staging
+            xsdb ivinstall.tcl
         fi
     )
 
