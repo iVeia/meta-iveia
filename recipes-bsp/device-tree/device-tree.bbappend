iVeia/meta-iveia

commit eb3aa43b5c41bede3c34fab28b94efc54d835c8f

ivinstall: allow for empty ivio/ioboard

recipes-bsp/device-tree/device-tree.bbappend
@@ -47,8 +47,10 @@ do_configure_append() {
         echo "#include <${IVIO}.dtsi>" >> ${COMBO_DTS}
     fi
 
-    # DTS overlays for each ioboard
-    cp ${WORKDIR}/ivio/*_overlay.dts ${DT_FILES_PATH}
+	if [ -d ${WORKDIR}/ivio ]; then
+    	# DTS overlays for each ioboard
+    	cp ${WORKDIR}/ivio/*_overlay.dts ${DT_FILES_PATH}
+	fi
 }
 
 do_deploy() {

recipes-core/images/files/ivinstall-doc
@@ -238,7 +238,7 @@ SUPPORTED BOARDS
         $MACHINE
 
     IO boards supported:
-$(echo $IOBOARDS | xargs -n1 | sed 's/^/        /')
+$(if [ -n "$IOBOARD" ]; then echo $IOBOARDS | xargs -n1 | sed 's/^/        /'; else echo "        none"; fi)
 
 EXAMPLES
 

recipes-core/images/ivinstall.bb
@@ -33,12 +33,14 @@ do_compile() {
 
     # Extract IOBOARD names from devicetree/*_overlay.dtbo files.
     IOBOARDS=""
-    for i in ${DEPLOY_DIR_IMAGE}/devicetree/*_overlay.dtbo; do
+	for i in $(find devicetree/ -name *_overlay.dtbo); do
         IOBOARD=$(basename "$i")
         IOBOARD="${IOBOARD%_overlay.dtbo}"
         IOBOARDS+="${IOBOARD} "
     done
-    INSERT_VARS+="IOBOARDS='${IOBOARDS}'\n"
+	if [ -n "${IOBOARDS}" ]; then
+    	INSERT_VARS+="IOBOARDS='${IOBOARDS}'\n"
+	fi
 
     . "${B}/${PN}.versions"
     INSERT_VARS+="IVEIA_META_BUILD_HASH='${IVEIA_META_BUILD_HASH}'\n"

commit 2be444757c6c35005088ec489eec131ea82cc5fd

ivinstall: fix to allow empty ivio/ioboard
Partial revert of previous commit eb3aa43b5c41bede3c34fab28b94efc54d835c8f, which broke the
help text listing available IOBOARDS (it always displayed none).

recipes-bsp/device-tree/device-tree.bbappend
@@ -47,10 +47,10 @@ do_configure_append() {
         echo "#include <${IVIO}.dtsi>" >> ${COMBO_DTS}
     fi
 
-	if [ -d ${WORKDIR}/ivio ]; then
-    	# DTS overlays for each ioboard
-    	cp ${WORKDIR}/ivio/*_overlay.dts ${DT_FILES_PATH}
-	fi
+    if [ -d ${WORKDIR}/ivio ]; then
+        # DTS overlays for each ioboard
+        cp ${WORKDIR}/ivio/*_overlay.dts ${DT_FILES_PATH}
+    fi
 }
 
 do_deploy() {

recipes-core/images/files/ivinstall-doc
@@ -238,7 +238,7 @@ SUPPORTED BOARDS
         $MACHINE
 
     IO boards supported:
-$(if [ -n "$IOBOARD" ]; then echo $IOBOARDS | xargs -n1 | sed 's/^/        /'; else echo "        none"; fi)
+$(echo $IOBOARDS | xargs -n1 | sed 's/^/        /')
 
 EXAMPLES
 

recipes-core/images/ivinstall.bb
@@ -33,14 +33,16 @@ do_compile() {
 
     # Extract IOBOARD names from devicetree/*_overlay.dtbo files.
     IOBOARDS=""
-	for i in $(find devicetree/ -name *_overlay.dtbo); do
+    for i in $(find ${DEPLOY_DIR_IMAGE}/devicetree/ -name "*_overlay.dtbo"); do
         IOBOARD=$(basename "$i")
         IOBOARD="${IOBOARD%_overlay.dtbo}"
         IOBOARDS+="${IOBOARD} "
     done
-	if [ -n "${IOBOARDS}" ]; then
-    	INSERT_VARS+="IOBOARDS='${IOBOARDS}'\n"
-	fi
+    if [ -n "${IOBOARDS}" ]; then
+        INSERT_VARS+="IOBOARDS='${IOBOARDS}'\n"
+    else
+        INSERT_VARS+="IOBOARDS=none\n"
+    fi
 
     . "${B}/${PN}.versions"
     INSERT_VARS+="IVEIA_META_BUILD_HASH='${IVEIA_META_BUILD_HASH}'\n"