iVeia/meta-iveia

commit dc8048350e5d715a7e7a546e5d135160d3f9335b

add /etc/os-release

recipes-core/images/iveia-image-common.inc
@@ -26,3 +26,13 @@ IMAGE_INSTALL += "kernel-module-iv-ocp"
 
 IMAGE_CLASSES_remove = "image-types-xilinx-qemu qemuboot-xilinx"
 
+
+# create /etc/os-release with build timestamp and git hash for meta-iveia 
+META_IVEIA_VER_DIR := "${THISDIR}"  
+gen_os_release() {
+        echo "date: $(date)" >> ${IMAGE_ROOTFS}/etc/os-release
+        META_IVEIA_COMMIT=`git -C ${META_IVEIA_VER_DIR} describe --dirty --match=NeVeRmAtCh --always`
+        echo "meta-iveia: ${META_IVEIA_COMMIT}" >> ${IMAGE_ROOTFS}/etc/os-release
+}
+IMAGE_PREPROCESS_COMMAND += "gen_os_release; "
+

commit fe615317af16f977adcec48fc52e4d3ee3418eae

unify version handling
Versions are now generated in one place (${META_IVEIA_VERSIONS_FILE}) on EVERY
build, and consistently to stamp the binaries.  In addition, version info is
now stored at /etc/os-release (this was broken in dc8048350e5d715 when using
newer versions of git).

Also:
- Multiple meta-iveia-* layers are now supported.
- SRC_BUILD_HASH is now based on the simpler ${PV} (which has the same info)
- Required targets were moved from ivinstall-full to common (e.g. `parted`)

Note: z7 builds have been built, but not tested on actual hardware (z7's use a
different patch for the FSBL).

See VERSIONS.md for more info.

classes/iveia-version-header.bbclass
@@ -5,44 +5,19 @@
 #           IVEIA_VERSION_HEADER_FILE (as an absolute path).
 #       2. a shell script sourceable versions file IVEIA_VERSIONS_FILE, at
 #           location defined below.
+#
+# See also VERSIONS.md
+#
 
-META_DIR := "${THISDIR}"
 IVEIA_VERSIONS_FILE = "${B}/${PN}.versions"
-
-#
-# Because a recipe may be patched and would therefore report the wrong "git
-# describe", we get the git version just before patch time.  In addition, to
-# show that the recipe has been mucked with, we check for tainted forced builds
-# via Yocto, and if found we append a tainted label to the revision
-#
-# For the meta layer version this doesn't apply, so we get the version at
-# compile time.
-#
-do_patch[prefuncs] += "do_post_patch_version"
-do_post_patch_version() {
-    if (cd "${S}"; git describe --tags); then
-        GIT_REV=$(cd "${S}" && git describe --long --tags)
-    else
-        GIT_REV="UNKNOWN"
-    fi
-    echo 'SRC_BUILD_HASH="'"${GIT_REV}"'"' > ${WORKDIR}/src_build_hash
-}
 
 do_compile[prefuncs] += "do_pre_compile_version"
 do_pre_compile_version () {
-    META_BUILD_HASH=$(cd ${META_DIR} && git describe --long --tags --dirty)
-    BUILD_DATE=$(date -u "+%F_%T")
-
-    if [ -f ${WORKDIR}/src_build_hash ]; then
-        . ${WORKDIR}/src_build_hash
-    fi
-    if [ -z "${SRC_BUILD_HASH}" ]; then
-        SRC_BUILD_HASH="NA"
-    fi
-
+    source ${META_IVEIA_VERSIONS_FILE}
+    
     TAINTED=""
     ls ${STAMP}.*.taint 2>/dev/null && TAINTED="(tainted)"
-    SRC_BUILD_HASH="${SRC_BUILD_HASH}${TAINTED}"
+    SRC_BUILD_HASH="${PV}${TAINTED}"
 
     #
     # If IVEIA_VERSION_HEADER_FILE is given, create version header file.
@@ -56,29 +31,27 @@ do_pre_compile_version () {
     #
     if [ -n "${IVEIA_VERSION_HEADER_FILE}" ]; then
         DECL='static char * __attribute__((used)) UID_734176a5a68b860afbe2293ae7ee22fd'
-        GREPPABLE_SRC_BUILD_HASH="GREPPABLE_SRC_BUILD_HASH=${SRC_BUILD_HASH}"
-        GREPPABLE_META_BUILD_HASH="GREPPABLE_META_BUILD_HASH=${META_BUILD_HASH}"
-        GREPPABLE_BUILD_DATE="GREPPABLE_BUILD_DATE=${BUILD_DATE}"
-        GREPPABLE_MACHINE="GREPPABLE_MACHINE=${MACHINE}"
         cat <<-EOF | sed 's/^ *//' > "${IVEIA_VERSION_HEADER_FILE}"
-            ${DECL}_1 = "${GREPPABLE_SRC_BUILD_HASH}";
-            ${DECL}_2 = "${GREPPABLE_META_BUILD_HASH}";
-            ${DECL}_3 = "${GREPPABLE_BUILD_DATE}";
-            ${DECL}_4 = "${GREPPABLE_MACHINE}";
+            ${DECL}_1 = "GREPPABLE_SRC_BUILD_HASH=${SRC_BUILD_HASH}";
+            ${DECL}_2 = "GREPPABLE_BUILD_DATE=${IVEIA_BUILD_DATE}";
+            ${DECL}_3 = "GREPPABLE_MACHINE=${MACHINE}";
             #define IVEIA_SRC_BUILD_HASH "${SRC_BUILD_HASH}"
-            #define IVEIA_META_BUILD_HASH "${META_BUILD_HASH}"
-            #define IVEIA_BUILD_DATE "${BUILD_DATE}"
+            #define IVEIA_BUILD_DATE "${IVEIA_BUILD_DATE}"
             #define IVEIA_MACHINE "${MACHINE}"
 			EOF
+        for i in `seq $IVEIA_NUM_LAYERS`; do
+            INDIRECT_LAYER=IVEIA_META_${i}_LAYER
+            INDIRECT_BUILD_HASH=IVEIA_META_${i}_BUILD_HASH
+            echo "#define ${INDIRECT_LAYER} \"${!INDIRECT_LAYER}\"" \
+                >> "${IVEIA_VERSION_HEADER_FILE}"
+            echo "#define ${INDIRECT_BUILD_HASH} \"${!INDIRECT_BUILD_HASH}\"" \
+                >> "${IVEIA_VERSION_HEADER_FILE}"
+        done
     fi
 
     # Shell-script sourceable versions file.
-    cat <<-EOF | sed 's/^ *//' > "${IVEIA_VERSIONS_FILE}"
-        IVEIA_SRC_BUILD_HASH="${SRC_BUILD_HASH}"
-        IVEIA_META_BUILD_HASH="${META_BUILD_HASH}"
-        IVEIA_BUILD_DATE="${BUILD_DATE}"
-        IVEIA_MACHINE="${MACHINE}"
-		EOF
+    echo "IVEIA_SRC_BUILD_HASH=\"${SRC_BUILD_HASH}\"" > "${IVEIA_VERSIONS_FILE}"
+    cat "${META_IVEIA_VERSIONS_FILE}" >> "${IVEIA_VERSIONS_FILE}"
 }
 
 inherit deploy

classes/meta-iveia-version.bbclass
@@ -1,0 +1,31 @@
+addhandler on_build_start
+
+META_IVEIA_VERSIONS_FILE := "${DEPLOY_DIR_IMAGE}/versions.txt"
+
+on_build_start() {
+    import re
+    bblayers = d.getVar('BBLAYERS').split()
+    pattern = "/meta-iveia(-[^/]*)?$"
+    iveia_bblayers = [l for l in bblayers if re.search(pattern, l)]
+
+    from subprocess import run
+    from datetime import datetime as dt 
+    
+    import os
+    versions_file = d.getVar("META_IVEIA_VERSIONS_FILE")
+    os.makedirs(os.path.dirname(versions_file), exist_ok=True)
+    with open(versions_file, 'w') as f:
+        f.write('IVEIA_MACHINE="{}"\n'.format(d.getVar("MACHINE")))
+        f.write('IVEIA_BUILD_DATE="{}"\n'.format(dt.now().strftime("%Y-%m-%d_%H:%M:%S")))
+        f.write('IVEIA_NUM_LAYERS="{}"\n'.format(len(iveia_bblayers)))
+        for i, layer_path in enumerate(iveia_bblayers):
+            layer_name = layer_path.split('/')[-1]
+            f.write('IVEIA_META_{}_LAYER="{}"\n'.format(i + 1, layer_name))
+
+            gitcmd = "git -C {} describe --long --tags --dirty".format(layer_path)
+            completetion = run(gitcmd, shell=True, capture_output=True)
+            first_line = completetion.stdout.decode().split('\n')[0]
+            f.write('IVEIA_META_{}_BUILD_HASH="{}"\n'.format(i + 1, first_line))
+}
+
+on_build_start[eventmask] = "bb.event.BuildStarted"

conf/layer.conf
@@ -16,3 +16,5 @@ HOSTTOOLS += "parted mount losetup"
 
 PACKAGE_CLASSES = "package_ipk"
 
+USER_CLASSES += "meta-iveia-version"
+

recipes-bsp/fsbl-firmware/files/iveia-version-banner-zynq.patch
@@ -1,17 +1,17 @@
-From 9e444e625724dd84033ea7d4e28b208c9f743459 Mon Sep 17 00:00:00 2001
+From 22f848bfd5686a8589a1b98b8aeab93cd8c4c0ca Mon Sep 17 00:00:00 2001
 From: Brian Silverman <bsilverman@iveia.com>
-Date: Tue, 8 Jun 2021 15:28:52 -0400
+Date: Tue, 7 Mar 2023 10:14:56 -0500
 Subject: [PATCH] iveia version banner for zynq
 
 ---
- lib/sw_apps/zynq_fsbl/src/main.c | 8 ++++++--
- 1 file changed, 6 insertions(+), 2 deletions(-)
+ lib/sw_apps/zynq_fsbl/src/main.c | 24 ++++++++++++++++++++++--
+ 1 file changed, 22 insertions(+), 2 deletions(-)
 
 diff --git a/lib/sw_apps/zynq_fsbl/src/main.c b/lib/sw_apps/zynq_fsbl/src/main.c
-index 43b5ce1..eed1e56 100644
+index 62a5b4dd64..88b6e34321 100644
 --- a/lib/sw_apps/zynq_fsbl/src/main.c
 +++ b/lib/sw_apps/zynq_fsbl/src/main.c
-@@ -133,6 +133,7 @@
+@@ -113,6 +113,7 @@
  #include "xstatus.h"
  #include "fsbl_hooks.h"
  #include "xtime_l.h"
@@ -19,7 +19,7 @@ index 43b5ce1..eed1e56 100644
  
  #ifdef XPAR_XWDTPS_0_BASEADDR
  #include "xwdtps.h"
-@@ -286,10 +287,13 @@ int main(void)
+@@ -266,10 +267,29 @@ int main(void)
  	/*
  	 * Print the FSBL Banner
  	 */
@@ -31,10 +31,26 @@ index 43b5ce1..eed1e56 100644
  			__DATE__,__TIME__);
 +	xil_printf("Machine:     iVeia %s\r\n", IVEIA_MACHINE);
 +	xil_printf("Src commit:  %s\r\n", IVEIA_SRC_BUILD_HASH);
-+	xil_printf("Meta commit: %s\r\n", IVEIA_META_BUILD_HASH);
++	#ifdef IVEIA_META_1_LAYER
++		xil_printf("Layer (%s) commit: %s\r\n", IVEIA_META_1_LAYER, IVEIA_META_1_BUILD_HASH);
++	#else
++		xil_printf("Layer commit: unknown\r\n");
++	#endif
++	#ifdef IVEIA_META_2_LAYER
++		xil_printf("Layer (%s) commit: %s\r\n", IVEIA_META_2_LAYER, IVEIA_META_2_BUILD_HASH);
++	#endif
++	#ifdef IVEIA_META_3_LAYER
++		xil_printf("Layer (%s) commit: %s\r\n", IVEIA_META_3_LAYER, IVEIA_META_3_BUILD_HASH);
++	#endif
++	#ifdef IVEIA_META_4_LAYER
++		xil_printf("Layer (%s) commit: %s\r\n", IVEIA_META_4_LAYER, IVEIA_META_4_BUILD_HASH);
++	#endif
++	#if IVEIA_NUM_LAYERS > 4
++		#error More than four meta-iveia* layers not supported
++	#endif
  
  #ifdef XPAR_PS7_DDR_0_S_AXI_BASEADDR
  
 -- 
-2.7.4
+2.25.1
 

recipes-bsp/fsbl-firmware/files/iveia-version-banner.patch
@@ -1,17 +1,17 @@
-From 0ae2831bdf9a3e590019d9597d9ddeb1b0ce8d76 Mon Sep 17 00:00:00 2001
+From 1ab1c7a41b54d9b94b173aa46fed131a7f201997 Mon Sep 17 00:00:00 2001
 From: Brian Silverman <bsilverman@iveia.com>
-Date: Tue, 17 Mar 2020 17:35:27 -0400
+Date: Mon, 6 Mar 2023 19:39:48 -0500
 Subject: [PATCH] iveia version banner
 
 ---
- lib/sw_apps/zynqmp_fsbl/src/xfsbl_main.c | 7 +++++++
- 1 file changed, 7 insertions(+)
+ lib/sw_apps/zynqmp_fsbl/src/xfsbl_main.c | 25 ++++++++++++++++++++++++
+ 1 file changed, 25 insertions(+)
 
 diff --git a/lib/sw_apps/zynqmp_fsbl/src/xfsbl_main.c b/lib/sw_apps/zynqmp_fsbl/src/xfsbl_main.c
-index 4dc1eaa..21f687f 100644
+index d38f799e91..f0398de286 100644
 --- a/lib/sw_apps/zynqmp_fsbl/src/xfsbl_main.c
 +++ b/lib/sw_apps/zynqmp_fsbl/src/xfsbl_main.c
-@@ -51,6 +51,7 @@
+@@ -34,6 +34,7 @@
  #include "xfsbl_hw.h"
  #include "xfsbl_main.h"
  #include "bspconfig.h"
@@ -19,17 +19,37 @@ index 4dc1eaa..21f687f 100644
  
  /************************** Constant Definitions *****************************/
  
-@@ -360,6 +361,10 @@ void XFsbl_PrintFsblBanner(void )
-	XFsbl_Printf(DEBUG_GENERAL, "MultiBootOffset: 0x%0x\r\n",
-		XFsbl_In32(CSU_CSU_MULTI_BOOT));
-
+@@ -351,6 +352,30 @@ void XFsbl_PrintFsblBanner(void )
+ 	XFsbl_Printf(DEBUG_GENERAL, "MultiBootOffset: 0x%0x\r\n",
+ 		XFsbl_In32(CSU_CSU_MULTI_BOOT));
+ 
 +	XFsbl_Printf(DEBUG_PRINT_ALWAYS,"Machine:     iVeia %s\r\n", IVEIA_MACHINE);
 +	XFsbl_Printf(DEBUG_PRINT_ALWAYS,"Src commit:  %s\r\n", IVEIA_SRC_BUILD_HASH);
-+	XFsbl_Printf(DEBUG_PRINT_ALWAYS,"Meta commit: %s\r\n", IVEIA_META_BUILD_HASH);
++	#ifdef IVEIA_META_1_LAYER
++		XFsbl_Printf(DEBUG_PRINT_ALWAYS,"Layer (%s) commit: %s\r\n",
++		   IVEIA_META_1_LAYER, IVEIA_META_1_BUILD_HASH);
++	#else
++		XFsbl_Printf(DEBUG_PRINT_ALWAYS, "Layer commit: unknown\r\n");
++	#endif
++	#ifdef IVEIA_META_2_LAYER
++		XFsbl_Printf(DEBUG_PRINT_ALWAYS,"Layer (%s) commit: %s\r\n",
++		   IVEIA_META_2_LAYER, IVEIA_META_2_BUILD_HASH);
++	#endif
++	#ifdef IVEIA_META_3_LAYER
++		XFsbl_Printf(DEBUG_PRINT_ALWAYS,"Layer (%s) commit: %s\r\n",
++		   IVEIA_META_3_LAYER, IVEIA_META_3_BUILD_HASH);
++	#endif
++	#ifdef IVEIA_META_4_LAYER
++		XFsbl_Printf(DEBUG_PRINT_ALWAYS,"Layer (%s) commit: %s\r\n",
++		   IVEIA_META_4_LAYER, IVEIA_META_4_BUILD_HASH);
++	#endif
++	#if IVEIA_NUM_LAYERS > 4
++		#error More than four meta-iveia* layers not supported
++	#endif
 +
  	if(FsblInstance.ResetReason == XFSBL_PS_ONLY_RESET) {
  		XFsbl_Printf(DEBUG_GENERAL,"Reset Mode	:	PS Only Reset\r\n");
  	} else if (XFSBL_MASTER_ONLY_RESET == FsblInstance.ResetReason) {
 -- 
-2.7.4
+2.25.1
 

recipes-bsp/u-boot/files/iveia-init.c
@@ -596,7 +596,23 @@ int misc_init_r(void)
 {
     printf("Machine:     iVeia %s\n", IVEIA_MACHINE);
     printf("Src commit:  %s\n", IVEIA_SRC_BUILD_HASH);
-    printf("Meta commit: %s\n", IVEIA_META_BUILD_HASH);
+	#ifdef IVEIA_META_1_LAYER
+		printf("Layer (%s) commit: %s\n", IVEIA_META_1_LAYER, IVEIA_META_1_BUILD_HASH);
+	#else
+		printf("Layer commit: unknown\n");
+	#endif
+	#ifdef IVEIA_META_2_LAYER
+		printf("Layer (%s) commit: %s\n", IVEIA_META_2_LAYER, IVEIA_META_2_BUILD_HASH);
+	#endif
+	#ifdef IVEIA_META_3_LAYER
+		printf("Layer (%s) commit: %s\n", IVEIA_META_3_LAYER, IVEIA_META_3_BUILD_HASH);
+	#endif
+	#ifdef IVEIA_META_4_LAYER
+		printf("Layer (%s) commit: %s\n", IVEIA_META_4_LAYER, IVEIA_META_4_BUILD_HASH);
+	#endif
+	#if IVEIA_NUM_LAYERS > 4
+		#error More than four meta-iveia* layers not supported
+	#endif
 
     iv_ipmi_scan( gd->fdt_blob );
 

recipes-core/images/files/ivinstall-header.sh
@@ -160,9 +160,16 @@ extract_archive_to_TMPDIR()
 # If requested, display version and md5sums
 #
 if ((DO_VERSION)); then
-    [[ -n "$IVEIA_META_BUILD_HASH" || -n "$IVEIA_BUILD_DATE" ]] || \
-        error "INTERNAL ERROR: version not available"
-    echo "Meta commit: ${IVEIA_META_BUILD_HASH}"
+    if [[ -n "$IVEIA_NUM_LAYERS" ]]; then
+        for ((i = 1; i <= $IVEIA_NUM_LAYERS; i++)); do
+            INDIRECT_LAYER=IVEIA_META_${i}_LAYER
+            INDIRECT_BUILD_HASH=IVEIA_META_${i}_BUILD_HASH
+            echo "Layer (${!INDIRECT_LAYER}) commit: ${!INDIRECT_BUILD_HASH}"
+        done
+    else
+        echo "Versions unknown"
+    fi
+    [[ -z "$IVEIA_BUILD_DATE" ]] && IVEIA_BUILD_DATE="unknown"
     echo "Build date: ${IVEIA_BUILD_DATE}"
     echo
 

recipes-core/images/iveia-image-common.inc
@@ -26,13 +26,15 @@ IMAGE_INSTALL += "kernel-module-iv-ocp"
 
 IMAGE_CLASSES_remove = "image-types-xilinx-qemu qemuboot-xilinx"
 
+# Required for ivinstall:
+IMAGE_INSTALL += "parted"
+IMAGE_INSTALL += "e2fsprogs-e2fsck e2fsprogs-mke2fs e2fsprogs-tune2fs"
+IMAGE_INSTALL += "e2fsprogs-badblocks e2fsprogs-resize2fs"
+IMAGE_INSTALL += "util-linux"
 
 # create /etc/os-release with build timestamp and git hash for meta-iveia 
-META_IVEIA_VER_DIR := "${THISDIR}"  
 gen_os_release() {
-        echo "date: $(date)" >> ${IMAGE_ROOTFS}/etc/os-release
-        META_IVEIA_COMMIT=`git -C ${META_IVEIA_VER_DIR} describe --dirty --match=NeVeRmAtCh --always`
-        echo "meta-iveia: ${META_IVEIA_COMMIT}" >> ${IMAGE_ROOTFS}/etc/os-release
+     cp ${META_IVEIA_VERSIONS_FILE} ${IMAGE_ROOTFS}/etc/os-release
 }
 IMAGE_PREPROCESS_COMMAND += "gen_os_release; "
 

recipes-core/images/iveia-image-full.bb
@@ -9,10 +9,7 @@ IMAGE_INSTALL += "packagegroup-core-ssh-openssh"
 
 # Add on packages
 IMAGE_INSTALL += "base-files-extras"
-IMAGE_INSTALL += "rsync parted coreutils"
-IMAGE_INSTALL += "e2fsprogs-e2fsck e2fsprogs-mke2fs e2fsprogs-tune2fs e2fsprogs-badblocks e2fsprogs-resize2fs"
-
-IMAGE_INSTALL += "util-linux"
+IMAGE_INSTALL += "rsync coreutils"
 IMAGE_INSTALL += "i2c-tools"
 IMAGE_INSTALL += "strace"
 IMAGE_INSTALL += "lmsensors-sensors lmsensors-libsensors"

recipes-core/images/ivinstall.inc
@@ -35,7 +35,7 @@ do_clean() {
 do_compile() {
     INSERT_VARS="MACHINE='${MACHINE}'\n"
 
-    # Extract IOBOARD names from devicetree/*_overlay.dtbo files.
+    # Extract IOBOARD names from devicetree/ *_overlay.dtbo files.
     IOBOARDS=""
     for i in $(find ${DEPLOY_DIR_IMAGE}/devicetree/ -name "*_overlay.dtbo"); do
         IOBOARD=$(basename "$i")
@@ -48,8 +48,15 @@ do_compile() {
         INSERT_VARS="${INSERT_VARS}IOBOARDS='none'\n"
     fi
 
-    . "${B}/${PN}.versions"
-    INSERT_VARS="${INSERT_VARS}IVEIA_META_BUILD_HASH='${IVEIA_META_BUILD_HASH}'\n"
+    source "${META_IVEIA_VERSIONS_FILE}"
+    source "${B}/${PN}.versions"
+    INSERT_VARS="${INSERT_VARS}IVEIA_NUM_LAYERS='${IVEIA_NUM_LAYERS}'\n"
+    for i in `seq $IVEIA_NUM_LAYERS`; do
+        INDIRECT_LAYER=IVEIA_META_${i}_LAYER
+        INDIRECT_BUILD_HASH=IVEIA_META_${i}_BUILD_HASH
+        INSERT_VARS="${INSERT_VARS}${INDIRECT_LAYER}='${!INDIRECT_LAYER}'\n"
+        INSERT_VARS="${INSERT_VARS}${INDIRECT_BUILD_HASH}='${!INDIRECT_BUILD_HASH}'\n"
+    done
     INSERT_VARS="${INSERT_VARS}IVEIA_BUILD_DATE='${IVEIA_BUILD_DATE}'\n"
 
     # Insert variables defs, and then the header doc into the named lines of

recipes-kernel/linux/files/iveia-version-banner.patch
@@ -19,12 +19,28 @@ index 32b2a8affafd..2e4c36ddaa6f 100644
  
  #include <asm/io.h>
  #include <asm/bugs.h>
-@@ -866,6 +867,8 @@ asmlinkage __visible void __init __no_sanitize_address start_kernel(void)
+@@ -866,6 +867,24 @@ asmlinkage __visible void __init __no_sanitize_address start_kernel(void)
  	boot_cpu_init();
  	page_address_init();
  	pr_notice("%s", linux_banner);
 +	pr_notice("Src commit:  %s\n", IVEIA_SRC_BUILD_HASH);
-+	pr_notice("Meta commit: %s\n", IVEIA_META_BUILD_HASH);
++	#ifdef IVEIA_META_1_LAYER
++		pr_notice("Layer (%s) commit: %s\n", IVEIA_META_1_LAYER, IVEIA_META_1_BUILD_HASH);
++	#else
++		pr_notice("Layer commit: unknown\n");
++	#endif
++	#ifdef IVEIA_META_2_LAYER
++		pr_notice("Layer (%s) commit: %s\n", IVEIA_META_2_LAYER, IVEIA_META_2_BUILD_HASH);
++	#endif
++	#ifdef IVEIA_META_3_LAYER
++		pr_notice("Layer (%s) commit: %s\n", IVEIA_META_3_LAYER, IVEIA_META_3_BUILD_HASH);
++	#endif
++	#ifdef IVEIA_META_4_LAYER
++		pr_notice("Layer (%s) commit: %s\n", IVEIA_META_4_LAYER, IVEIA_META_4_BUILD_HASH);
++	#endif
++	#if IVEIA_NUM_LAYERS > 4
++		#error More than four meta-iveia* layers not supported
++	#endif
  	early_security_init();
  	setup_arch(&command_line);
  	setup_boot_config(command_line);