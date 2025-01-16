d2e3df518aaed826a140368f98b2dd2ccd942b17
lmaxwell <lmaxwell@iveia.com>
2025-01-16 16:52:18 -0500

    00108: fsbl: fix broken i2c-1
    the default iv_z8_init_before_hook configures i2c-1 to use MIO 8/9;
    this does not apply to 00108 (it uses MIO 36/37)

--- recipes-bsp/fsbl-firmware/files/00108.c
+++ recipes-bsp/fsbl-firmware/files/00108.c
@@ -10,3 +10,13 @@ int iv_z8_has_clock_chip()
     return 0;
 }
 
+void iv_z8_init_before_hook()
+{
+    // the default iv_z8_init_before_hook configures MIO 8/9 to be i2c-1;
+    // this is does not apply to 00108 (it uses MIO 36/37 for i2c-1)
+    //
+    // override the hook to do nothing
+    //
+    return;
+}
+
